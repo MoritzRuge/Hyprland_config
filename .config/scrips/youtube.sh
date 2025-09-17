165b9fc3d30329483635497ee53df4738ba17627
Moritz <ruge.moritz@gmail.com>
2025-09-18 00:46:56 +0200

    ADD - Youtube pull and watch script

---
+++ .config/scrips/youtube.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+output_path="$HOME/Downloads/"
+
+if [ -z "$1" ]; then
+  echo "Input Youtube link: "
+  read youtube_link
+else
+  youtube_link="$1"
+fi
+
+if [ -z "$youtube_link" ]; then
+  echo "Bitte link einfügen."
+  exit 0
+else
+  echo "Downloading Video"
+  echo "$youtube_link"
+
+  # Video herunterladen und den Pfad speichern
+  yt-dlp -o "$output_path%(title)s.%(ext)s" "$youtube_link"
+
+  filename=$(yt-dlp --get-filename -o "$output_path%(title)s.%(ext)s" "$youtube_link")
+
+  echo "Video heruntergeladen: $filename"
+  mpv "$filename"
+
+  echo "Deleting file: '$filename'"
+  rm -v "$filename"
+fi
