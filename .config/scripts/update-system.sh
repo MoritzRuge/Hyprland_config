b08677312877c70ddd91527d8b568ba37f4d0dd0
Moritz <ruge.moritz@gmail.com>
2025-09-21 19:49:24 +0200

    ADD - Waybar system update script

---
+++ .config/scripts/update-system.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+# Script to update the system
+
+checkupdates
+sleep 1
+
+while true; do
+  read -r -p "Do you wish to upgrade the system?(y/N): " answer
+  case $answer in
+  [Yy]*)
+    echo "Upgrading the system!"
+    sleep 0.5
+    sudo pacman -Syu --noconfirm
+    echo "Completed the system upgrade!"
+    sleep 1
+    exit 0
+    ;;
+  [Nn]*)
+    echo "Exiting script."
+    exit 0
+    ;;
+  *) echo "Please answer Y or N." ;;
+  esac
+done
