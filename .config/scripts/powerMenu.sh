da746b541aa42f7e5746733bb8dba01666f1bead
Moritz <ruge.moritz@gmail.com>
2025-10-28 10:20:39 +0100

    ADD - powerMenu.sh

---
+++ .config/scripts/powerMenu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+selection=$(echo "Sleep
+Log out
+Reboot
+Shutdown
+Shutdown Now" | wofi -dmenu)
+
+if [[ $selection == "Shutdown" ]]; then
+  hyprctl notify 3 30000 "rgb(ff0000)" "Shutdown scheduled"
+  sleep 30
+  systemctl poweroff
+elif [[ $selection == "Reboot" ]]; then
+  systemctl reboot
+elif [[ $selection == "Shutdown Now" ]]; then
+  systemctl poweroff
+elif [[ $selection == "Log out" ]]; then
+  loginctl terminate-user $USER
+elif [[ $selection == "Sleep" ]]; then
+  systemctl suspend
+else
+  hyprctl dismissnotify &>/dev/null
+  killall powerMenu.sh
+fi
